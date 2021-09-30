a981dd1099445311af6abe8618fb355e6a874540
dengqu2 <dengqu2@joyy.sg>
2021-09-30 11:10:05 +0800

    添加线程接口类

---
+++ thread/IThread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 邓区 on 2021/9/30.
+//
+
+#include "IThread.h"
---
+++ thread/IThread.h
@@ -0,0 +1,14 @@
+//
+// Created by 邓区 on 2021/9/30.
+//
+
+#ifndef CHTTP_ITHREAD_H
+#define CHTTP_ITHREAD_H
+
+
+class IThread {
+
+};
+
+
+#endif //CHTTP_ITHREAD_H
